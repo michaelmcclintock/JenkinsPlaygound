38ba212531506033df7bcb23a159bf82e32784b3
Michael McClintock <Mike.mcclintock@sonos.com>
2017-02-10 09:57:50 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,41 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "aws_key_path" {}
+variable "aws_key_name" {}
+
 variable "region" {
+  description = "EC2 Region for the VPC"
   default = "us-west-2"
+}
+
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        eu-west-1 = "ami-f1810f86" # ubuntu 14.04 LTS
+    }
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the whole VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr01" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr01" {
+    description = "CIDR for the Private Subnet"
+    default = "10.0.1.0/24"
+}
+
+variable "public_subnet_cidr02" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr02" {
+    description = "CIDR for the Private Subnet"
+    default = "10.0.1.0/24"
 }
